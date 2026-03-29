358c918d31abf7c780f1e9d2097d1bb2ec3648e9
yitao36 <yitao36g@gmail.com>
2026-03-30 03:18:36 +0800

    Add server.ts

---
+++ server.ts
@@ -0,0 +1,13 @@
+Deno.serve((req: Request) => {
+  // Create a URL object from the request URL string
+  const url = new URL(req.url);
+
+  // Access the URLSearchParams object
+  const params = url.searchParams;
+
+  // Get specific parameters
+  const flag = params.get("flag");
+  console.log(flag)
+
+  return new Response("You got hacked!")
+});
