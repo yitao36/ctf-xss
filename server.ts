a82c579898403b762b33031fa7c53d916bb9ce3f
yitao36 <yitao36g@gmail.com>
2026-03-30 04:40:07 +0800

    Fix localhost

--- server.ts
+++ server.ts
@@ -8,5 +8,5 @@ Deno.serve((req: Request) => {
   // Get specific parameters
   const flag = params.get("flag");
   console.log(flag)
-  return Response.redirect("https://localhost:3000", 302);
+  return Response.redirect("https://localhost:8080", 302);
 });
