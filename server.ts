19a092c5f6d70122b4480be56416fd6542de79e4
yitao36 <yitao36g@gmail.com>
2026-03-30 04:53:21 +0800

    Fix localhost

--- server.ts
+++ server.ts
@@ -8,5 +8,5 @@ Deno.serve((req: Request) => {
   // Get specific parameters
   const flag = params.get("flag");
   console.log(flag)
-  return Response.redirect("http://127.0.0.1/", 301);
+  return Response.redirect("/debug/healthcheck", 301);
 });
